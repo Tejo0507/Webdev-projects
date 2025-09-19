d45a7116a5e917aa524df21064bc80da366dd289
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-19 22:34:35 +0530

    Create script.js

---
+++ Claw Machine Game/script.js
@@ -0,0 +1,7 @@
+ const elements = {
+    clawMachine: document.querySelector('.claw-machine'),
+    box: document.querySelector('.box'),
+    collectionBox: document.querySelector('.collection-box'),
+    collectionArrow: document.querySelector('.collection-arrow'),
+    toys: [],
+  }

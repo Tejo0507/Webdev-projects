90d2690b10bfe5d40a3ca3076c5d86a894268d71
TEJO SRIDHAR M V S <tejosridhar.mvs@gmail.com>
2025-09-19 22:40:45 +0530

    Update script.js

--- Claw Machine Game/script.js
+++ Claw Machine Game/script.js
@@ -66,3 +66,41 @@
   const { height: machineBottomHeight, top: machineBottomTop } = document
     .querySelector('.machine-bottom')
     .getBoundingClientRect()
+  const maxArmLength = machineBottomTop - machineTop - machineBuffer.y
+
+  const adjustAngle = angle => {
+    const adjustedAngle = angle % 360
+    return adjustedAngle < 0 ? adjustedAngle + 360 : adjustedAngle
+  }
+
+  const randomN = (min, max) => {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  }
+
+  //* classes *//
+
+  class Button {
+    constructor({ className, action, isLocked, pressAction, releaseAction }) {
+      Object.assign(this, {
+        el: document.querySelector(`.${className}`),
+        isLocked,
+      })
+      this.el.addEventListener('click', action)
+      ;['mousedown', 'touchstart'].forEach(action =>
+        this.el.addEventListener(action, pressAction),
+      )
+      ;['mouseup', 'touchend'].forEach(action =>
+        this.el.addEventListener(action, releaseAction),
+      )
+
+      if (!isLocked) this.activate()
+    }
+    activate() {
+      this.isLocked = false
+      this.el.classList.add('active')
+    }
+    deactivate() {
+      this.isLocked = true
+      this.el.classList.remove('active')
+    }
+  }
